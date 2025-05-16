8340a36e6cb2d6ef3ef2dbc92fda027af9fe628b
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-16 15:20:32 -0400

    Pushing my solution for the #LinkedListTest problem

---
+++ linkedlist/LinkedListTest.java
@@ -0,0 +1,51 @@
+private static Node{
+    int val;
+    Node next;
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public Node insertNodeLastPosition(Node inputHead, int val){
+
+}
+
+public Node insertNodeAtFirst(Node inputHead, int val){
+
+}
+
+public Node insertAtPosition(Node inputHead, int val, int position){
+
+}
+
+public Node removeNode(Node inputHead, int position){
+
+}
+
+public Node removeLastNode(Node inputHead){
+
+}
+
+public void readLinkedList(Node inputHead){
+
+}
+
+public Node findNodeAtPosition(Node inputHead, int position){
+
+}
+
+public Node reverseList(Node inputHead) {
+}
+
+public boolean hasCycle(Node inputHead) {
+
+}
+
+public Node findStartOfCycle(Node inputHead){
+
+}
+
+public Node findMiddleNode(Node inputHead){
+
+}	
