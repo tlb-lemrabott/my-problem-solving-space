0f25989118a4527d3a2a20244e149e68e7148a8b
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-16 15:20:32 -0400

    Pushing my solution for the #TreeExample problem

---
+++ tree/TreeExample.java
@@ -0,0 +1,25 @@
+// Post-order Traversal
+// Pre-order Traversal
+// In-order Traversal 
+// Level-order traversal using queue: https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+// Search in BST
+// Insert/Delete in BST
+// Validate BST
+
+
+
+
+void preorder(TreeNode root)
+void inorder(TreeNode root)
+void postorder(TreeNode root)
+List<List<Integer>> levelOrder(TreeNode root) 
+void printTree(TreeNode root)  
+
+int maxDepth(TreeNode root) 
+boolean isSameTree(TreeNode p, TreeNode q)
+boolean isSymmetric(TreeNode root)
+boolean isBalanced(TreeNode root) https://leetcode.com/problems/balanced-binary-tree/description/
+int diameterOfBinaryTree(TreeNode root)
+boolean hasPathSum(TreeNode root, int sum) https://leetcode.com/problems/path-sum/description/
+List<List<Integer>> pathSum(TreeNode root, int sum)
