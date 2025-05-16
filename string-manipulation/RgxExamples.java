3fc65ab68d7d93931d24df44efcbcb0f8f6c0440
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-16 15:20:32 -0400

    Pushing my solution for the #RgxExamples problem

---
+++ string-manipulation/RgxExamples.java
@@ -0,0 +1,9 @@
+public static String replaceFirstOccurrence(String input, String original, String censored){
+    return input.replaceFirst(original, censored);
+}
+
+public static String replaceAllOccurrences(String input, String original, String censored){
+    return input.replaceAll(original, censored);
+}
+
+//
