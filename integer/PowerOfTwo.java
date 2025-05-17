6da36859e61b48ab4b27cbc9f212aac6c19e11e5
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-16 20:45:31 -0400

    Update PowerOfTwo file - Add new solution

--- integer/PowerOfTwo.java
+++ integer/PowerOfTwo.java
@@ -1,5 +1,17 @@
 https://leetcode.com/problems/power-of-two/description/
 
 public boolean isPowerOfTwo(int n) {
-        
+    int x = 31;
+    while (x >= 0){
+        if(Math.pow(2, x) == n){
+            System.out.println(x);
+            return true;
+        }
+        x--;
+    }
+    return false;
+}
+
+public boolean isPowerOfTwoUsingXAND(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
 }
