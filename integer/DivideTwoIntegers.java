189a9e6865f10473c23071d53a64ded62f87132d
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-16 22:08:45 -0400

    Add solution for DivideTwoIntegers problem

--- integer/DivideTwoIntegers.java
+++ integer/DivideTwoIntegers.java
@@ -1,5 +1,16 @@
 https://leetcode.com/problems/divide-two-integers/description/
 
 int divide(int dividend, int divisor) {
-        
+    if(dividend == Integer.MIN_VALUE && divisor < 0){
+        return Integer.MAX_VALUE;
+    }
+    boolean isNegative = (dividend < 0) ^ (divisor < 0);
+    long a = Math.abs((long) dividend);
+    long b = Math.abs((long) divisor);
+    int result = 0;
+    while (a >= 0){
+        a -= b;
+        result++;
+    }
+    return isNegative ? -result : result;
 }
