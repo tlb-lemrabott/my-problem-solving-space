7409739bfcddb4d7a95a3e5bafb4c702f78e5490
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-19 13:55:34 -0400

    Add solution for #convert!DArrayTo2DArray leetcode problem

--- arrays-manipulation/Convert1DArrayInto2DArray.java
+++ arrays-manipulation/Convert1DArrayInto2DArray.java
@@ -1,5 +1,10 @@
 https://leetcode.com/problems/convert-1d-array-into-2d-array/
 
 public int[][] construct2DArray(int[] original, int m, int n) {
-
+    if(original.length != n * m) return new int[0][0];
+    int[][] result = new int[n][m];
+    for(int i=0; i<original.length; i++){
+        result[i/n][i%n] = original[i];
+    }
+    return result;
 }
