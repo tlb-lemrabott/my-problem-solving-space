03ba19af98505832da2f3fa4c73cd17184c91e8d
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-19 07:59:51 -0400

    Fix array size error

--- dynamic-program/ClimbingStairs.java
+++ dynamic-program/ClimbingStairs.java
@@ -2,7 +2,7 @@
 
 public int climbingStairs(int n) {
     if(n <= 2) return n;
-    int []dp = new int[n];
+    int []dp = new int[n+1];
     dp[1] = 1;
     dp[2] = 2;
     for(int i=3; i<=n; i++){
