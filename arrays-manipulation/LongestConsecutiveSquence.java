1e11eb7d499edcfea3c0c01a5538ef33ec9c9247
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-19 12:59:01 -0400

    Add solution for the #longestConsecutiveSequence leetcode problem

--- arrays-manipulation/LongestConsecutiveSquence.java
+++ arrays-manipulation/LongestConsecutiveSquence.java
@@ -1,5 +1,21 @@
 https://leetcode.com/problems/longest-consecutive-sequence/description/
 
 public int longestConsecutive(int[] nums) {
-        
+    HashSet<Integer> set = new HashSet<>();
+    for(int num : nums){
+        set.add(num);
+    }
+    int count = 0;
+    for(int num : set){
+        if(!set.contains(num - 1)){
+            int currentNum = num;
+            int currentCount = 1;
+            while(set.contains(currentNum + 1)){
+                currentNum++;
+                currentCount++;
+            }
+            count = Math.max(count, currentCount);
+        }
+    }
+    return count;
 }
