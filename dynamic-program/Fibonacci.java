c0d8b7488a55d92d78ae72e88f8de36b429b2b72
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-19 08:01:33 -0400

    change array name

--- dynamic-program/Fibonacci.java
+++ dynamic-program/Fibonacci.java
@@ -1,12 +1,12 @@
 public int fibo(int n){
     if(n <= 1) return n;
-    int []arr = new int[n+1];
-    arr[0] = 0;
-    arr[1] = 1;
-    for(int i=0; i<=n; i++){
-        arr[i] = arr[i-1] + arr[i-2];
+    int []dp = new int[n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
     }
-    return arr[n];
+    return dp[n];
 }
 
 //Using HashMap with DP
