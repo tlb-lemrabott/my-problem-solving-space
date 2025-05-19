b3d891daa716c1f9f79c8515d08440ce9c4f8ac1
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-19 11:26:03 -0400

    Improving solution for equation x + y + z = target to return all possibilities

--- math-equations/MathExample.java
+++ math-equations/MathExample.java
@@ -1,3 +1,4 @@
+//Solution for equation x + y + z = number 
 public int[] solveEquation(int arr[], int number){
     //find x, y, z from arr, where x + y + z = number
     Arrays.sort(number);
@@ -18,9 +19,10 @@ public int[] solveEquation(int arr[], int number){
     return null;
 }
 
-public int[] solveEquation2(int arr[], int number){
+
+//Solution for equation x^2 + y^2+ z^2 = number 
+public int[] solveEquation(int arr[], int number){
     Arrays.sort(arr);
-    //find x, y, z from arr, where x^2 + y^2+ z^2 = number 
     for(int i=0; i < arr.length-2; i++){
         int x = number - (arr[i] * arr[i]);
         int left = i+1;
@@ -37,7 +39,30 @@ public int[] solveEquation2(int arr[], int number){
     }
     return null;
 }
+//Note Math.pow(x, 2) return double => it is hard to be used in this case.
 
 
-
-//Math.pow(x, 2) return double => it is hard to be used in this case.
+//Improving solution for equation x + y + z = target to return all possibilities
+public List<List<Integer>> solveEquation(int arr[], int number){
+    List<List<Integer>> result = new ArrayList<>();
+    for(int i=0; i < arr.length; i++){
+        if(i > 0 && arr[i] == arr[i-1]) continue;
+        int x = number - arr[i];
+        int left = i+1;
+        int right = arr.length - 1;
+        while(left < right){
+            if(x == arr[left] + arr[right]){
+                result.add(Arrays.asList(arr[i], arr[left], arr[right]));
+                while(left < right && arr[left] == arr[left+1]) left++;
+                while(left < right && arr[right] == arr[right-1]) right--;
+                left++;
+                right--;
+            }else if(x > arr[left] + arr[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+    return result;
+}
