cb135738872a7667ddb88cbe43a70aef89755968
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-20 11:51:18 -0400

    Submit solution for #RemoveDuplicate proble

--- arrays-manipulation/RemoveDuplicatesFromArray.java
+++ arrays-manipulation/RemoveDuplicatesFromArray.java
@@ -1,9 +1,13 @@
-
-public void removeDuplicatesMutated(int arr[]){
-
-}
-
-
-public int[] removeDuplicatesUnMutated(int arr[]){
-
+//Add Solution for #RemoveDuplicatesFromArray problem.
+public int[] removeDuplicates(int arr[]){
+    Set<Integer> set = new LinkedHashSet<>();
+    for(int num : arr){
+        set.add(num);
+    }
+    int []result = new int[set.size()];
+    int i = 0;
+    for(int num : set){
+        result[i++] = num;
+    }
+    return result;
 }
