50ce02322d4f63992e2ae0415a5af85db6a001bc
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-05-20 11:52:56 -0400

    Submit my solution for the #TopKFrequentElement leetcode problem

--- arrays-manipulation/TopKFrequentElements.java
+++ arrays-manipulation/TopKFrequentElements.java
@@ -1,5 +1,17 @@
 https://leetcode.com/problems/top-k-frequent-elements/
 
 public int[] topKFrequent(int[] nums, int k) {
-
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int num : nums){
+        map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+    PriorityQueue<Map.Entry<Integer, Integer>> maxHeap = new PriorityQueue<>(
+        (a, b) -> b.getValue() - a.getValue()
+    );
+    maxHeap.addAll(map.entrySet());
+    int []result = new int[k];
+    for(int i=0; i<k; i++){
+        result[i] = maxHeap.poll().getKey();
+    }
+    return result;
 }
