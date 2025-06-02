9b75ded46194fb038824e87f8c4378d5a08a7941
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-06-02 10:15:12 -0400

    submit solution for #CanPlaceFlowers leetcode problem.

---
+++ arrays-manipulation/CanPlaceFlowers.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+
+public boolean canPlaceFlowers(int[] flowerbed, int n) {
+    int count = 0;
+    for (int i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
+            (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+            flowerbed[i] = 1;
+            count++;
+            if (count >= n) return true;
+        }
+    }
+    return count >= n;
+}
