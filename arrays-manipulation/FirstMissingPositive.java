bc28b57de3949047063b9a720547d803160d1e33
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-06-02 11:44:41 -0400

    Submit solution for #FirstMissingPositive leetcode problem.

---
+++ arrays-manipulation/FirstMissingPositive.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/first-missing-positive/submissions/1651829843/?envType=problem-list-v2&envId=m3r184od
+
+public int firstMissingPositive(int[] nums) {
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            int temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
+}
