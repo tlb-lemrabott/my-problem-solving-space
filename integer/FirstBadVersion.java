6b3600a9b697e176b92c537d488b778bdacb7955
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-06-06 00:43:02 -0400

    Submit solution for the #FirstBadVersion leetcode problem.

---
+++ integer/FirstBadVersion.java
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/first-bad-version/?envType=problem-list-v2&envId=m3r184od
+
+public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
