3f3f93f420ddc56d9d52a497325a82c2b7d18603
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-06-09 23:34:26 -0400

    Submit solution for the #RansomeNote leetcode problem.

---
+++ string-manipulation/RansomNote.java
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/ransom-note/description/?envType=problem-list-v2&envId=m3r184od
+
+public boolean canConstruct(String ransomNote, String magazine) {
+    int[] letters = new int[26];
+    for (char c : magazine.toCharArray()) {
+        letters[c - 'a']++;
+    }
+    for (char c : ransomNote.toCharArray()) {
+        if (letters[c - 'a'] == 0) {
+            return false;
+        }
+        letters[c - 'a']--;ß
+    }
+    return true;
+}
