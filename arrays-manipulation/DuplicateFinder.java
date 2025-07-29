92920a8fdb688fc8020384324e5291bcc9c382f0
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-29 13:33:34 -0400

    Add solution for the DuplicateFinder problem

--- arrays-manipulation/DuplicateFinder.java
+++ arrays-manipulation/DuplicateFinder.java
@@ -6,7 +6,21 @@ public static List<Integer> findDuplicates(int[] arr) {
     for (int num : arr) {
         if (!seen.add(num)) {
             // If add returns false, it's already seen → duplicate
-        duplicates.add(num);
+            duplicates.add(num);
+        }
     }    
     return new ArrayList<>(duplicates);
+}
+
+
+public static List<Integer> findDuplicates(int[] arr){
+    Set<Integer> stack = new HashSet<>();
+    List<Integer> duplicates = new ArrayList();
+
+    for(int num : arr){
+        if(!stack.add(num)){
+            duplicates.add(num);
+        }
+    }
+    return duplicates;
 }
