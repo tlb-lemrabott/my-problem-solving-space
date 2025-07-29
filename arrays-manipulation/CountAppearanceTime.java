85bab15cb392f5419669be513b003b5d4aef11ae
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-29 13:31:53 -0400

    Update solution for CountAppearanceTime problem

--- arrays-manipulation/CountAppearanceTime.java
+++ arrays-manipulation/CountAppearanceTime.java
@@ -7,3 +7,18 @@ public static HashMap<Integer, Integer> countAppearanceTime(int []nums){
 }
 
 // int []arr = map.values().stream().mapToInt(x -> x).toArray();
+
+
+public static HashMap<Integer, Integer> countAppearanceTime(int []nums){
+    HashMap<Integer, Integer> counterMap = new HashMap<>();
+    for(int num : nums){
+        counterMap.put(num, counterMap.getOrDefault(num, 0) + 1);
+    }
+    return counterMap;
+}
+
+for(Map.Entry<Integer, Integer> entry : counterMap.entrySet()){
+    System.out.print(entry.getKey() + ", " + entry.getValue());
+}
+
+
