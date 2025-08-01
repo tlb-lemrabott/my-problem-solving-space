3f9750d7ccbc69d02bfd95d72710c616f39cc955
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-07-31 23:42:04 -0400

    Update Singleton pattern implementation

--- object-oriented/SingletonPatter.java
+++ object-oriented/SingletonPatter.java
@@ -4,7 +4,7 @@
 public class Singleton{
     private static Singleton instance = null;
     private Singleton(){}
-    public static Synchronized Singleton getInstance{
+    public static Synchronized Singleton getInstance(){
         if(instance == null){
             return new Singleton();
         }
