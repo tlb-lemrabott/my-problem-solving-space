732832b938654b375229a1e3da8bced665a174e1
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-08-18 23:28:13 -0400

    Add table accounts schema to bankingDb

--- sql/bankingDB.sql
+++ sql/bankingDB.sql
@@ -1,5 +1,15 @@
+Table: customers
 | customer\_id | name    | city     |
 | ------------ | ------- | -------- |
 | 1            | Alice   | New York |
 | 2            | Bob     | Chicago  |
 | 3            | Charlie | Boston   |
+
+Table: accounts
+| account\_id | customer\_id | account\_type | balance |
+| ----------- | ------------ | ------------- | ------- |
+| 101         | 1            | savings       | 5000    |
+| 102         | 1            | checking      | 2000    |
+| 103         | 2            | savings       | 8000    |
+| 104         | 3            | checking      | 3000    |
+
