61c1452013b915b1c9d7c91096d26571ae9f6419
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-08-18 23:27:03 -0400

    Add bankingDb tables schema

---
+++ sql/bankingDB.sql
@@ -0,0 +1,5 @@
+| customer\_id | name    | city     |
+| ------------ | ------- | -------- |
+| 1            | Alice   | New York |
+| 2            | Bob     | Chicago  |
+| 3            | Charlie | Boston   |
