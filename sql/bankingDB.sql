9616354bf3b72c4fcb69a1f6b8a58cfc8a3da832
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-08-18 23:29:17 -0400

    Add table transactions schema to bankingDb

--- sql/bankingDB.sql
+++ sql/bankingDB.sql
@@ -13,3 +13,11 @@ Table: accounts
 | 103         | 2            | savings       | 8000    |
 | 104         | 3            | checking      | 3000    |
 
+Table: accounts
+| account\_id | customer\_id | account\_type | balance |
+| ----------- | ------------ | ------------- | ------- |
+| 101         | 1            | savings       | 5000    |
+| 102         | 1            | checking      | 2000    |
+| 103         | 2            | savings       | 8000    |
+| 104         | 3            | checking      | 3000    |
+
