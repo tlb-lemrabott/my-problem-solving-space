511494c68e4da0a609492291dc5a29383c261711
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-09-23 14:54:11 -0400

    Update Factorial and add LetterCasePermutation solutions

--- recursion/Factorial.java
+++ recursion/Factorial.java
@@ -1,3 +1,9 @@
+// https://leetcode.com/problems/factorial-trailing-zeroes/description/
+
 public static int findFactorial(int n){
-    return n;
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return n * findFactorial(n-1);
+    }
 }
