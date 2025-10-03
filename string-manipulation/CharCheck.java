e4498c4161f138c7edb1437a9d757773d43ffbb0
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-03 16:05:37 -0400

    Add character classification demo

---
+++ string-manipulation/CharCheck.java
@@ -0,0 +1,20 @@
+//Given a string determine if each character is letter of digit.
+
+
+public class CharCheck {
+    public static void main(String[] args) {
+        String input = "Abc123$%";
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (Character.isLetter(c)) {
+                System.out.println(c + " is a letter");
+            } else if (Character.isDigit(c)) {
+                System.out.println(c + " is a digit");
+            } else {
+                System.out.println(c + " is neither letter nor digit");
+            }
+        }
+    }
+}
