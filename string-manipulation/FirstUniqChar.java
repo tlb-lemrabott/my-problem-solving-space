106e4a6a19afd1b492cf6771997aa2f9f4f4896c
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-15 23:02:01 -0400

    feat: add string-manipulation/FirstUniqChar.java

---
+++ string-manipulation/FirstUniqChar.java
@@ -0,0 +1,20 @@
+// Find the First Non-Repeating Character in a String
+// Given a string, find the first non-repeating character in it and return its index. If it doesn’t exist, return -1.
+// Example questions:
+// Consider a string where all characters are lowercase alphabets.
+// Example 1:
+// Input:
+// s = 'helloworld'
+// Output:
+// first_uniq_char(s) -> 0
+// In the string ‘helloworld’, the first non-repeating character is ‘h’, which is at index 0.
+// Example 2:
+// Input:
+// s = 'concussion'
+// Output:
+// first_uniq_char(s) -> 4
+// In the string ‘concussion’, the first non-repeating character is ‘u’, which is at index 4.
+
+public int first_uniq_char(String s){
+    return 0;
+}
