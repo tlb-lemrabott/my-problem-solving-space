d40310a9481df4a9d75f9a91961f6724a4b51372
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-15 23:06:56 -0400

    feat: add arrays-manipulation/FlattenNDimensionalArrayTo1DArray.java

---
+++ arrays-manipulation/FlattenNDimensionalArrayTo1DArray.java
@@ -0,0 +1,17 @@
+// Flatten N-Dimensional Array to 1D Array
+// You are given an N-dimensional array (a nested list) and your task is to convert it into a 1D array. The N-dimensional array can have any number of nested lists and each nested list can contain any number of elements. The elements in the nested lists are integers. Write a function that takes an N-dimensional array as input and returns a 1D array.
+// Example 1:
+// Input:
+// array = [1, [2, 3], [4, [5, 6]], 7]
+// Output:
+// flatten_array(array) -> [1, 2, 3, 4, 5, 6, 7]
+// Example 2:
+// Input:
+// array = [[1, 2], [3, 4], [5, 6]]
+// Output:
+// flatten_array(array) -> [1, 2, 3, 4, 5, 6]
+
+public int [] flatten_array(int arr[][]){
+    int [] result = new int[arr.length];
+    return result;
+}
