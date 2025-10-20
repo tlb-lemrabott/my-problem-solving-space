4fe4e5898e4600c4e8a0b92945e5a3323e9b278e
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-19 23:06:47 -0400

    Update FirstNonRepeatingElement.java: commit latest changes

--- arrays-manipulation/FirstNonRepeatingElement.java
+++ arrays-manipulation/FirstNonRepeatingElement.java
@@ -1,12 +1,22 @@
-public int firstNonRepeatingElement(int arr[]){
-    Map<Integer, Integer> mapElement = new HashMap<>();
-    for(int i=0; i<arr.length; i++){
-        mapElement.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-    }
-    for(Map<Integer, Integer> entry: entrySet()){
-        if(map.get(entry) == 1){
-            return entry;
+public static int firstNonRepeatingElement(int arr[]){
+        Map<Integer, Integer> mapElement = new HashMap<>();
+        for(int i=0; i<arr.length; i++){
+            mapElement.put(arr[i], mapElement.getOrDefault(arr[i], 0) + 1);
         }
+        for (int num : arr) {
+            if (mapElement.get(num) == 1) {
+                return num;
+            }
+        }
+        return -1;
     }
-    return -1;
-}
+
+    public static void main(String[] args) {
+        int[] arr1 = {4, 5, 1, 2, 0, 4};
+        int[] arr2 = {1, 2, 2, 1, 3, 4, 4};
+        int[] arr3 = {7, 7, 7, 7};
+
+        System.out.println("First non-repeating element in arr1: " + firstNonRepeatingElement(arr1));
+        System.out.println("First non-repeating element in arr2: " + firstNonRepeatingElement(arr2));
+        System.out.println("First non-repeating element in arr3: " + firstNonRepeatingElement(arr3));
+    }
