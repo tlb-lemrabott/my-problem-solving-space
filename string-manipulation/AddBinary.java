54a5a59fa5ef3fe2e22f52389b37fcf54b98fd83
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-20 23:57:11 -0400

    string-manipulation: implement addBinary in Java (replace C++ snippet)

--- string-manipulation/AddBinary.java
+++ string-manipulation/AddBinary.java
@@ -1,5 +1,20 @@
-https://leetcode.com/problems/add-binary/description/
+// https://leetcode.com/problems/add-binary/description/
+// Adds two binary strings and returns the binary sum.
+public static String addBinary(String a, String b) {
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    int carry = 0;
+    StringBuilder sb = new StringBuilder(Math.max(a.length(), b.length()) + 1);
 
-string addBinary(string a, string b) {
-        
+    while (i >= 0 || j >= 0 || carry != 0) {
+        int bitA = (i >= 0) ? (a.charAt(i) - '0') : 0;
+        int bitB = (j >= 0) ? (b.charAt(j) - '0') : 0;
+        int sum = bitA + bitB + carry;
+
+        sb.append((char) ('0' + (sum & 1)));
+        carry = sum >> 1;
+        i--; j--;
+    }
+
+    return sb.reverse().toString();
 }
